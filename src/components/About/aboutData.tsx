cf9c947ef9091b8f1be37e37bb48d273fbd0d397
jeremingolla <jeremingolla03@gmail.com>
2023-10-04 17:06:23 -0300

    Portfolio

---
+++ src/components/About/aboutData.tsx
@@ -0,0 +1,63 @@
+import { SiNextdotjs, SiRedux, SiNodedotjs, SiExpress, SiPostgresql, SiAstro, SiJavascript, SiTailwindcss } from "react-icons/si";
+import { FaReact, FaHtml5, FaCss3Alt } from "react-icons/fa";
+const aboutData=[
+    {
+        icon:<FaReact size="40"/>,
+        name: "React"
+
+},
+{
+    icon:<FaHtml5  size="40"/>,
+    name: "HTML"
+
+},
+{
+    icon:<SiJavascript size="40"/>,
+    name: "JS"
+
+},
+{
+    icon:<SiTailwindcss size="40"/>,
+    name: "Tailwind"
+
+},
+{
+    icon:<FaCss3Alt size="40"/>,
+    name: "Css"
+
+},
+{
+    icon:<SiAstro size="40"/>,
+    name: "Astro"
+
+},
+{
+    icon:<SiNextdotjs size="40"/>,
+    name: "NextJS"
+
+},
+{
+    icon:<SiPostgresql size="40"/>,
+    name: "PostgresSQL"
+
+},
+{
+    icon:<SiNodedotjs size="40"/>,
+    name: "NodeJS"
+
+},
+{
+    icon: <SiRedux size="40"/>,
+    name: "ReduxJS"
+
+},
+{
+    icon:<SiExpress  size="40"/>,
+    name: "ExpressJS"
+
+},
+]
+
+export default aboutData
+
+
